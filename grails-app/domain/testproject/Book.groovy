dcd85eefea84e7bedeb49a5b461fe52c96ec5ac0
soheil <s.s@gmail.com>
2014-01-18 16:26:24 +0000

    all my chnages

--- grails-app/domain/testproject/Book.groovy
+++ grails-app/domain/testproject/Book.groovy
@@ -2,6 +2,19 @@ package testproject
 
 class Book {
 
+    String name
+    String title
+    String code
+    String year
+
+
     static constraints = {
     }
+
+
+    def getData()
+    {
+        
+    }
+
 }
